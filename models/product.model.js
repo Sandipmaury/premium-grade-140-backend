74511961c6fe333973acbdf940c6d106edf5b8db
sandip <103632232+Sandipmaury@users.noreply.github.com>
2022-11-09 11:05:16 +0530

    userSchema done

--- models/product.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const productSchema = mongoose.Schema({
-  title: { type: String, require: true },
-  s: { type: String, require: true },
-  password: { type: String, require: true },
-});
-
-export const UserModel = mongoose.model("products", productSchema);
